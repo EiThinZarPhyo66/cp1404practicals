0d8303f467a8f4e24c219700c0138003e7d2a155
eithinzarphyo <eithinzarphyo@my.jcu.edu.au>
2023-07-22 14:55:11 +0800

    Complete the file

--- Week 2/password_stars.py
+++ Week 2/password_stars.py
@@ -1,7 +1,18 @@
 MINIMUM_LENGTH = 6
-password = input("Enter a password: ")
 
-while len(password) < MINIMUM_LENGTH:
-    print("Password is too short. Minimum length is", MINIMUM_LENGTH)
+
+def main():
+    password = get_password()
+    print("*" * len(password))
+
+
+def get_password():
+    """Get the password from the user"""
     password = input("Enter a password: ")
-print("*" * len(password))
+    while len(password) < MINIMUM_LENGTH:
+        print("Password is too short. Minimum length is", MINIMUM_LENGTH)
+        password = input("Enter a password: ")
+    return password
+
+
+main()
