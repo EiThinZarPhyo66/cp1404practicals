692326d187fcad06f077fa36d077acc15281575c
eithinzarphyo <eithinzarphyo@my.jcu.edu.au>
2023-07-22 14:07:51 +0800

    Refactor the code

---
+++ Week 2/score.py
@@ -0,0 +1,19 @@
+def main():
+    score = float(input("Enter score: "))
+    result = determine_result(score)
+    print(result)
+
+
+def determine_result(score):
+    if score < 0 or score > 100:
+        result = "Invalid score"
+    elif score >= 90:
+        result = "Excellent"
+    elif score >= 50:
+        result = "Passable"
+    else:
+        result = "Bad"
+    return result
+
+
+main()
