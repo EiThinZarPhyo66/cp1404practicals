85004593b386b71044afd6302bfa9c7d0427f9bf
eithinzarphyo <eithinzarphyo@my.jcu.edu.au>
2023-07-22 14:54:09 +0800

    Update the file

--- Week 2/temperatures.py
+++ Week 2/temperatures.py
@@ -1,17 +1,19 @@
-def main():
-    MENU = """C - Convert Celsius to Fahrenheit
+MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
+
+
+def main():
     print(MENU)
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "C":
             celsius = float(input("Celsius: "))
-            fahrenheit = convert_celsius_to_fahrenheit(celsius)
+            fahrenheit = calculate_fahrenheit(celsius)
             print(f"Result: {fahrenheit:.2f} F")
         elif choice == "F":
             fahrenheit = float(input("Fahrenheit: "))
-            celsius = convert_fahrenheit_to_celsius(celsius, fahrenheit)
+            celsius = calculate_celsius(fahrenheit)
             print(f"Result: {celsius:.2f} C")
         else:
             print("Invalid option")
@@ -20,12 +22,14 @@ Q - Quit"""
     print("Thank you.")
 
 
-def convert_fahrenheit_to_celsius(celsius, fahrenheit):
+def calculate_celsius(fahrenheit):
+    """Convert fahrenheit to celsius."""
     celsius = 5 / 9 * (fahrenheit - 32)
     return celsius
 
 
-def convert_celsius_to_fahrenheit(celsius):
+def calculate_fahrenheit(celsius):
+    """Convert_celsius_to_fahrenheit"""
     fahrenheit = celsius * 9.0 / 5 + 32
     return fahrenheit
 
