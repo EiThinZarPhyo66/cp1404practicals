03fc92ad3465a0c7ed142a0e24caea316be59d0a
eithinzarphyo <eithinzarphyo@my.jcu.edu.au>
2023-07-23 23:37:13 +0800

    Update the file

--- Week 2/password_stars.py
+++ Week 2/password_stars.py
@@ -3,15 +3,20 @@ MINIMUM_LENGTH = 6
 
 def main():
     password = get_password()
+    while len(password) < MINIMUM_LENGTH:
+        print("Password is too short. Minimum length is", MINIMUM_LENGTH)
+        password = get_password()
+    print_asterisks(password)
+
+
+def print_asterisks(password):
+    """Display asterisks as much as user's password"""
     print("*" * len(password))
 
 
 def get_password():
-    """Get the password from the user"""
+    """Get password from users."""
     password = input("Enter a password: ")
-    while len(password) < MINIMUM_LENGTH:
-        print("Password is too short. Minimum length is", MINIMUM_LENGTH)
-        password = input("Enter a password: ")
     return password
 
 
