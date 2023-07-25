068f5649f9a8b763ad4d3b134bc6cd4b459aaddf
eithinzarphyo <eithinzarphyo@my.jcu.edu.au>
2023-07-25 14:41:27 +0800

    Complete the file

---
+++ Week 3/randoms.py
@@ -0,0 +1,13 @@
+import random
+
+print(random.randint(5, 20))  # line 1
+print(random.randrange(3, 10, 2))  # line 2
+print(random.uniform(2.5, 5.5))  # line 3
+print(random.randint(1, 100))
+
+# On line 1, the smallest number I could have seen was 5 and the largest was 20.
+
+# On line 2, the smallest number I could have seen was 3 and the largest was 10.
+#The start value was 3 and the step value was 2. Therefore, line 2 couldn't produced a 4.
+
+# On line 3, the smallest number I could have seen was 2.5 and the largest was 5.5.
