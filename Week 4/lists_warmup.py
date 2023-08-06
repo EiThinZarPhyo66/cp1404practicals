e2f6a0ff15b064649ee7b86ca912f86e4b4145a8
eithinzarphyo <eithinzarphyo@my.jcu.edu.au>
2023-08-06 13:12:07 +0800

    Complete the file

--- Week 4/lists_warmup.py
+++ Week 4/lists_warmup.py
@@ -1,12 +1,14 @@
 numbers = [3, 1, 4, 1, 5, 9, 2]
 # 1
 numbers[0] = "ten"
+print(numbers)
 
 # 2
 numbers[-1] = 1
+print(numbers)
 
 # 3
-numbers[2:]
+print(numbers[2:])
 
 # 4
-9 in numbers
+print(9 in numbers)
