98d41105db276f51433b32fc162e94f522f9ec46
eithinzarphyo <eithinzarphyo@my.jcu.edu.au>
2023-08-29 16:29:33 +0800

    Add the file

---
+++ Week 7/guitar.py
@@ -0,0 +1,30 @@
+CURRENT_YEAR = 2023
+VINTAGE_AGE = 50
+
+
+class Guitar:
+    """Represent information about guitar"""
+
+    def __init__(self, name="", year=0, cost=0):
+        """Create a Guitar from the given values"""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Return string representation of a Guitar."""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        """Calculate how old the guitar is in years."""
+        return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        """Determine whether a guitar is vintage or not."""
+        return self.get_age() >= VINTAGE_AGE
+
+    def __lt__(self, other):
+        """Sort the list of guitars by released year."""
+        return self.year < other.year
+
+
