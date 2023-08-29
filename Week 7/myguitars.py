004e4a020066eac217badcc67bd51dd701927492
eithinzarphyo <eithinzarphyo@my.jcu.edu.au>
2023-08-29 16:31:43 +0800

    Complete the file

--- Week 7/myguitars.py
+++ Week 7/myguitars.py
@@ -18,5 +18,18 @@ def main():
     for guitar in guitars:
         print(guitar)
 
+    name = input("Name: ").title()
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar_to_add = Guitar(name, year, cost)
+        guitars.append(guitar_to_add)
+        print(f"{guitar_to_add}, added.")
+        name = input("Name: ").title()
+
+    with open(FILENAME, "w") as out_file:
+        for guitar in guitars:
+            out_file.write(str(guitar) + "\n")
+
 
 main()
