0123f10a2f25310f279fe2dfa95f9ac19a3e5bcd
eithinzarphyo <eithinzarphyo@my.jcu.edu.au>
2023-09-01 22:05:33 +0800

    refactor save_projects function

--- Week 7/project_management.py
+++ Week 7/project_management.py
@@ -13,16 +13,26 @@ def main():
     while choice != "q":
         if choice == "l":
             load_projects(FILENAME)
-            # elif choice == "s":
-            # elif choice == "d":
-            # elif choice == "f":
-            # elif choice == "a":
-            # elif choice == "u":
-            # else:
+        elif choice == "s":
+            save_projects(FILENAME)
+        # elif choice == "d":
+        # elif choice == "f":
+        # elif choice == "a":
+        # elif choice == "u":
+        else:
             print("Invalid input")
         print(MENU)
         choice = input(">>> ").upper()
     print("Thank you for using custom-built project management software.")
+
+
+def save_projects(filename):
+    """Save update projects into projects.txt"""
+    with open(filename, "w") as out_file:
+        out_file.write("Name\tStart Date\tPriority\tEstimate\tCompletion\n")
+        for project in projects:
+            out_file.write(f"{project.name}\t{project.start_date.strftime('%d/%m/%Y')}\t{project.priority}"
+                           f"\t{project.cost_estimate}\t{project.completion_percentage}\n")
 
 
 def load_projects(filename):
